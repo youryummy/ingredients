2e78f6022d87833eda47a0bc833ee93e9b2cc344
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2023-01-11 12:57:36 +0100

    fix: Added support for integration tests

--- tests/setup.test.js
+++ tests/setup.test.js
@@ -1,4 +1,26 @@
 import { logger } from "@oas-tools/commons";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
 logger.configure({ level: "off" });
 process.env.NODE_ENV = "test";
+
+// Populate test db and cleanup after integration tests
+if (process.argv.includes("tests/integration")) {
+    const password = bcrypt.hashSync("Test1234", 10);
+    
+    mongoose.set('strictQuery', false);
+    await mongoose.connect("mongodb://localhost:27017/test", {connectTimeoutMS: 3000, serverSelectionTimeoutMS: 3000 }).then(async () => {
+
+        // Cleans db after tests
+        const oldExit = process.exit;
+        process.exit = async (code) => {
+            await mongoose.connection.db.dropCollection("ingredients");                                                         
+            await mongoose.disconnect();
+            oldExit(code);
+        };
+    }).catch((err) => {
+        console.log("Failed to connect to test db: ", err.message);
+        process.exit(1);
+    });
+}
