b1f6696f98f6d1e0682e4c4613a043583a9f52bd
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2023-01-17 00:02:36 +0100

    feat: Added validation for URLs in the Ingredient schema

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -3,11 +3,29 @@ import { Schema, model} from "mongoose";
 const ingredientSchema = new Schema({
     _id: Schema.Types.ObjectId,
     nombre: String,
-    url: String,
+    url: {
+      type: String,
+      validate: {
+        validator: (value) => (/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/).test(value),
+        message: () => `Invalid URL`
+      }
+    },
     creado_por: String,
     marca: String,
-    imagen: String,
-    imagen_peq: String,
+    imagen: {
+      type: String,
+      validate: {
+        validator: (value) => (/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/).test(value),
+        message: () => `Invalid URL`
+      }
+    },
+    imagen_peq: {
+      type: String,
+      validate: {
+        validator: (value) => (/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/).test(value),
+        message: () => `Invalid URL`
+      }
+    },
   });
 
 export default model('Ingredient', ingredientSchema, 'ingredients');
